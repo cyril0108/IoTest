eac9022ceb07aef1543b1453c9cff5eeb8583d50
unknown <>
2015-12-25 13:44:43 +0800

    udp test code

--- mytest.cpp
+++ mytest.cpp
@@ -1,8 +1,36 @@
 #include <QObject>
 #include <QDebug>
+#include <QUdpSocket>
 #include "mytest.h"
 
 
 void MyTest::testIOT() {
     qDebug() << "test!" << endl;
+    if(udpSocket == NULL) initSocket();
+
+}
+
+void MyTest::initSocket()
+{
+    udpSocket = new QUdpSocket(this);
+    udpSocket->bind(QHostAddress::Any, 3479);
+
+    connect(udpSocket, SIGNAL(readyRead()),
+            this, SLOT(readPendingDatagrams()));
+}
+
+void MyTest::readPendingDatagrams()
+{
+    while (udpSocket->hasPendingDatagrams()) {
+        QByteArray datagram;
+        datagram.resize(udpSocket->pendingDatagramSize());
+        QHostAddress sender;
+        quint16 senderPort;
+
+        udpSocket->readDatagram(datagram.data(), datagram.size(),
+                                &sender, &senderPort);
+
+        //processTheDatagram(datagram);
+        qDebug() << datagram.data();
+    }
 }
--- mytest.h
+++ mytest.h
@@ -1,4 +1,5 @@
 #include <QObject>
+#include <QUdpSocket>
 #ifndef MYTEST_H
 #define MYTEST_H
 
@@ -6,8 +7,15 @@
 class MyTest : public QObject
 {
     Q_OBJECT
+
+private:
+    QUdpSocket *udpSocket;
+
 public slots:
     void testIOT();
+    void readPendingDatagrams();
+public:
+    void initSocket();
 };
 
 #endif // MYTEST_H
